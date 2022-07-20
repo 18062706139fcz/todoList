e502f24cfd817d8346a81d22c8a21f4ea2379074
18062706139fcz <2279549769@qq.com>
2022-07-20 21:42:54 +0800

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,118 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+// setup是干嘛的？
+// 内联渲染，无需注册，直接使用
+// 需要引入Vue中的computed onMounted ref
+import {computed,onMounted,ref} from 'vue';
+// 定义相关变量｜todo｜todoList
+let inputValue = ref<string>("");
+type Todo = {
+  value: string,
+  done: boolean
+};
+let todoLists = ref<Todo[]>([
+  { value: 'Ryker想吃饭饭', done:false},
+  { value: 'Ryker想拿牌牌', done:true},
+  { value: 'Ryker想打人人', done:false},
+]);
+const handleAdd = () => {
+  if (inputValue.value) {
+    const list:Todo = { value:inputValue.value, done:false};
+    todoLists.value.push(list);
+    inputValue.value = '';
+  }
+
+};
+// 
+const handleRemove = (index: number) => {
+  todoLists.value.splice(index, 1);
+};
+// remove
+const handleClear = () => {
+  todoLists.value = todoLists.value.filter((item) => !item.done)
+};
+// const selectAll = (f:boolean) => {
+//   todoLists.value.forEach((item) => {
+//     item.done = f;
+//   })
+// }
+// let ano:boolean = false;
+// const Judge = () => {
+//   selectAll(!ano)
+//   ano = !ano
+// }
+const active = computed(() => {
+  return todoLists.value.filter((item) => item.done).length
+});
+const all = computed(() => {
+  return todoLists.value.length;
+});
+// const fix = () => {
+//   if(active.value === all.value - 1) {
+//     ano = true;
+//     selectAll(!ano);
+//   } else if (active.value === 1) {
+//     ano = false;
+//     selectAll(!ano)
+//   }
+// };
+const isAllDone = computed({
+  get() {
+    return active.value == all.value;
+  },
+  set(value: boolean) {
+    return todoLists.value.forEach((item) => item.done = value)
+  }
+})
+const input = ref(null);
+onMounted(() => {
+  if(input.value !== null)
+    input.value.focus()
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<div class="container">
+    <input 
+    ref="input"
+    type="text" 
+    class="styled"
+    v-model="inputValue"
+    >
+    <button @click="handleAdd">add</button>
+    <button @click="handleClear">remove</button>
+    <ul v-if="todoLists.length">
+    <li
+    v-for="(todo, index) in todoLists"
+    :key="index"
+    :class="{done: todo.done}"
+    >
+    <input type="checkbox" v-model="todo.done" />
+    {{ todo.value }}
+    </li>
+    <span>全选</span><input type="checkbox" v-model="isAllDone"/>
+    {{ `${active}/${all}` }}
+    </ul>
+    <div v-else>nothing here</div>
+</div>
 </template>
 
+<!-- container实现了垂直居中 -->
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+.container {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 400px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+li {
+  text-align: left;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+span {
+  text-align: left;
+}
+.done {
+  text-decoration: line-through;
 }
 </style>
---
+++ src/components/uselessCompare.vue
@@ -0,0 +1,53 @@
+
+<script setup lang="ts">
+import { computed, onMounted, ref } from "vue";
+let inputValue = ref<string>("");
+type Todo = {
+  titile: string;
+  isDone: boolean;
+};
+let todoList = ref<Todo[]>([
+  { titile: "速冻鱼吃饭", isDone: false },
+  { titile: "速冻鱼睡觉", isDone: false },
+  { titile: "速冻鱼学习vue3", isDone: true },
+]);
+const handlerAdd = () => {
+  if (inputValue.value) {
+    const todo: Todo = { titile: inputValue.value, isDone: false };
+    todoList.value.push(todo);
+    inputValue.value = "";
+  }
+};
+const input = ref(null);
+onMounted(() => {
+  if (input.value !== null)
+    // @ts-ignore
+    input.value.focus();
+});
+</script>
+
+<template>
+  <div class="container">
+    <input
+      ref="input"
+      type="text"
+      v-model="inputValue"
+    />
+    <button @click="handlerAdd">add</button>
+    <button >clear</button>
+    <ul v-if="todoList.length">
+    </ul>>
+  </div>
+</template>
+
+<style>
+.container {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+.done {
+  text-decoration: line-through;
+}
+</style>
